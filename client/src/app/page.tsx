e874e26ee7a8484cf043533f039e22b021ea628b
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-23 01:17:25 +0200

    remove old template code

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,9 @@
-import { promisedTimeout } from "@/utils/utils";
 import { Home } from "@/views/Home";
 
-const getData = async () => {
-  await promisedTimeout(3000);
-  return await fetch("https://google.com").then((res) => res.text());
-};
-
 export default async function page() {
-  const data = await getData();
-
   return (
     <main>
       <Home />
-      {data}
     </main>
   );
 }

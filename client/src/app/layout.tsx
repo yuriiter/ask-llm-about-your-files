de320f004a9dbcb1175fd59f95148bc343111a12
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-23 22:03:21 +0200

    remove everything unnecessary from root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,25 +1,17 @@
-import { auth } from "@/auth";
 import { Providers } from "@/components/Providers";
 import StyledComponentsRegistry from "@/components/StyledComponentsRegistry";
 import "@/styles/main.scss";
-// import axios from "axios";
-
-// axios.defaults.withCredentials = true;
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
-
-  // const [colorscheme] = useColorscheme();
-
   return (
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
-          <Providers>{!session ? "Not authorized" : children}</Providers>
+          <Providers>{children}</Providers>
         </StyledComponentsRegistry>
       </body>
     </html>

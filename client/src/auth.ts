aff2cfaaa9ba771badb4dc011c3c63e6b0a738e3
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-23 22:02:04 +0200

    use Google provider

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -1,21 +1,19 @@
 import NextAuth from "next-auth";
 import { NextAuthConfig } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const config = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_APP_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
-      // Your existing signIn callback logic
       return true;
     },
     async session({ session, token, user }) {
-      // Your existing session callback logic
       return session;
     },
   },

087094f0993a73c39022dc489dd87e2bea0d9a9d
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-23 22:56:32 +0200

    remove unused modules

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -5,15 +5,6 @@ export const cn = (...classNames: (string | undefined | null | boolean)[]) =>
     )
     .join(" ");
 
-export const mod = (a: number, n: number): number => {
-  return a - n * Math.floor(a / n);
-};
-
-export const clamp = (min: number, max: number, value: number) =>
-  Math.min(Math.max(value, min), max);
-
-export const copy = <T>(ob: T) => JSON.parse(JSON.stringify(ob)) as T;
-
 export const promisedTimeout = (duration: number) =>
   new Promise((resolve) => {
     setTimeout(() => resolve(1), duration);

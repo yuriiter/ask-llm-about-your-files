2b8b44b55207ee516f9b092e7c72583d08bdf69f
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-24 20:11:20 +0200

    move grpc proto file to project root

--- vector-db/compile_proto.sh
+++ vector-db/compile_proto.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 source venv/bin/activate
 python -m grpc_tools.protoc \
-    --proto_path=./api/protos \
+    --proto_path=../proto/ \
     --python_out=. \
     --grpc_python_out=. \
-    ./api/protos/vector_service.proto
+    ../proto/vector_service.proto
 

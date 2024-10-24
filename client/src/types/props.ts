e1208bd83d2eb3f1df0bb0227215844bb94471d2
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-24 15:50:18 +0200

    remove all custom components and utilities related to styled-components

--- client/src/types/props.ts
+++ client/src/types/props.ts
@@ -1,20 +1,14 @@
 import { CSSProperties, ElementType, PropsWithChildren } from "react";
-import { StyledObject } from "styled-components";
 import { PolymorphicComponentProps } from "./polymorphicProps";
 
 export type PropsWithClassName = {
   className?: string;
 };
 
-export type PropsWithSx = {
-  sx?: StyledObject;
-};
-
 export type StandardProps = PropsWithChildren &
-  PropsWithClassName &
-  PropsWithSx & { style?: CSSProperties };
+  PropsWithClassName & { style?: CSSProperties };
 
 export type BoxProps<C extends ElementType> = PolymorphicComponentProps<
   C,
-  PropsWithClassName & PropsWithSx
+  PropsWithClassName
 >;
